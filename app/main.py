27169d699f069e50924c4f1968633c0414ad8f5c
tarun8524 <tarun.resoluteai@gmail.com>
2026-03-18 00:01:04 +0530

    intial commit

--- app/main.py
+++ app/main.py
@@ -24,7 +24,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*"],  # or your Vercel domain
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
